b5e0c2243c6947ef3589d819f08620392582c430
Alex Lalejini <amlalejini@gmail.com>
2019-11-07 15:24:31 -0500

    move flow handler out of signalgp hardware

--- source/SignalGPLinearProgram.h
+++ source/SignalGPLinearProgram.h
@@ -19,103 +19,6 @@
 #include "linear_signalgp_utils.h"
 
 namespace emp { namespace signalgp {
-  //EXEC_STATE_T
-// TAG_T
-// CUSTOM_COMPONENT_T
-
-  // TODO - hide this visibility so it doesn't leak
-  // namespace LinearProgramSignalGP_impl {
-
-  //   /// Library of flow types.
-  //   /// e.g., WHILE, IF, ROUTINE, et cetera
-  //   /// NOTE - I'm not sure that I'm a fan of how this is organized/named/setup.
-  //   /// BASIC: if statements (for now)
-  //   enum class FlowType : size_t { BASIC, WHILE_LOOP, ROUTINE, CALL };
-
-  //   /// Everything the execution stepper needs to know to manage (open, close, break)
-  //   /// any of the execution flow types.
-  //   struct FlowInfo {
-  //     FlowType type;    ///< Flow type ID?
-  //     size_t mp;        ///< Module pointer. Which module is being executed?
-  //     size_t ip;        ///< Instruction pointer. Which instruction is executed?
-  //     size_t begin;     ///< Where does the flow begin?
-  //     size_t end;       ///< Where does the flow end?
-
-  //     FlowInfo(FlowType _type, size_t _mp=(size_t)-1, size_t _ip=(size_t)-1,
-  //              size_t _begin=(size_t)-1, size_t _end=(size_t)-1)
-  //       : type(_type), mp(_mp), ip(_ip), begin(_begin), end(_end) { ; }
-
-  //     size_t GetBegin() const { return begin; }
-  //     size_t GetEnd() const { return end; }
-  //     size_t GetMP() const { return mp; }
-  //     size_t GetIP() const { return ip; }
-  //     FlowType GetType() const { return type; }
-  //   };
-
-  //   /// State information for a function call.
-  //   template<typename MEMORY_STATE_T>
-  //   struct CallState {
-  //     MEMORY_STATE_T memory;            ///< Memory local to this call state.
-  //     emp::vector<FlowInfo> flow_stack; ///< Stack of 'Flow' (a stack of fancy read heads)
-  //     bool circular;                    ///< Should call wrap when IP goes off end? Or, implicitly return?
-
-  //     CallState(const MEMORY_STATE_T & _mem=MEMORY_STATE_T(), bool _circular=false)
-  //       : memory(_mem), flow_stack(), circular(_circular) { ; }
-
-  //     bool IsFlow() const { return !flow_stack.empty(); }
-
-  //     emp::vector<FlowInfo> & GetFlowStack() { return flow_stack; }
-
-  //     FlowInfo & GetTopFlow() {
-  //       emp_assert(flow_stack.size());
-  //       return flow_stack.back();
-  //     }
-
-  //     bool IsCircular() const { return circular; }
-
-  //     MEMORY_STATE_T & GetMemory() { return memory; }
-
-  //     // --- For your convenience shortcuts: ---
-  //     /// Set the instruction pointer of the 'flow' at the top of the flow stack.
-  //     void SetIP(size_t i) { emp_assert(flow_stack.size()); flow_stack.back().ip = i; }
-
-  //     /// Set the module pointer at the top of the flow stack.
-  //     void SetMP(size_t m) { emp_assert(flow_stack.size()); flow_stack.back().mp = m; }
-
-  //     /// Get the instruction pointer at the top of the flow stack.
-  //     size_t GetIP() const { emp_assert(flow_stack.size()); return flow_stack.back().ip; }
-
-  //     /// Get the module pointer at the top of the flow stack.
-  //     size_t GetMP() const { emp_assert(flow_stack.size()); return flow_stack.back().mp; }
-
-  //     /// Get a mutable reference to the instruction pointer from the top of the flow stack.
-  //     size_t & IP() { emp_assert(flow_stack.size()); return flow_stack.back().ip; }
-
-  //     /// Get a mutable reference to the module pointer from the top of the flow stack.
-  //     size_t & MP() { emp_assert(flow_stack.size()); return flow_stack.back().mp; }
-  //   };
-
-  //   /// Execution State. TODO - add label?
-  //   template<typename MEMORY_MODEL_T>
-  //   struct ExecState {
-  //     using memory_state_t = typename MEMORY_MODEL_T::memory_state_t;
-  //     using call_state_t = CallState<memory_state_t>;
-  //     emp::vector<call_state_t> call_stack;   ///< Program call stack.
-
-  //     /// Empty out the call stack.
-  //     void Clear() { call_stack.clear(); }
-
-  //     /// Get a reference to the current (top) call state on the call stack.
-  //     /// Requires the call stack to be not empty.
-  //     call_state_t & GetTopCallState() {
-  //       emp_assert(call_stack.size(), "Cannot get top call state from empty call stack.");
-  //       return call_stack.back();
-  //     }
-
-  //     /// Get a mutable reference to the entire call stack.
-  //     emp::vector<call_state_t> & GetCallStack() { return call_stack; }
-  //   };
-  // }
 
   template<typename MEMORY_MODEL_T,
            typename TAG_T=emp::BitSet<16>,
@@ -141,6 +44,7 @@ namespace emp { namespace signalgp {
     using call_state_t = typename exec_state_t::call_state_t;
     using flow_t = lsgp_utils::FlowType;
     using flow_info_t = lsgp_utils::FlowInfo;
+    using flow_handler_t = lsgp_utils::FlowHandler<this_t, exec_state_t>;
 
     // using exec_state_t = ExecState;
     using tag_t = TAG_T;
@@ -163,64 +67,67 @@ namespace emp { namespace signalgp {
     using inst_t = typename program_t::inst_t;
     using inst_lib_t = InstructionLibrary<this_t, inst_t, InstProperty>;
 
-    using fun_end_flow_t = std::function<void(this_t&, exec_state_t &)>;                   // note - pass hardware down?
-    using fun_open_flow_t = std::function<void(this_t&, exec_state_t &, const flow_info_t &)>;
+    // using fun_end_flow_t = std::function<void(this_t&, exec_state_t &)>;                   // note - pass hardware down?
+    // using fun_open_flow_t = std::function<void(this_t&, exec_state_t &, const flow_info_t &)>;
+
+    using fun_end_flow_t = typename flow_handler_t::fun_end_flow_t;
+    using fun_open_flow_t = typename flow_handler_t::fun_open_flow_t;
 
     /// Flow control management. Maintains a mapping from FlowTypes (above) to
     /// a flow control structure. Additionally, provides an interface to open, break,
     /// and close flow.
     /// Every type of execution 'flow' has a 'FlowControl' structure that specifies
     /// how to open, close, and break the flow.
-    struct FlowHandler {
-      struct FlowControl {
-        fun_open_flow_t open_flow_fun;
-        fun_end_flow_t close_flow_fun;
-        fun_end_flow_t break_flow_fun;
-      };
+    // struct FlowHandler {
+    //   struct FlowControl {
+    //     fun_open_flow_t open_flow_fun;
+    //     fun_end_flow_t close_flow_fun;
+    //     fun_end_flow_t break_flow_fun;
+    //   };
 
-      /// Mapping from flow type to flow control structure.
-      // todo - could change this to an array because we know things at compile time
-      std::map<flow_t, FlowControl> lib = { {flow_t::BASIC, FlowControl()},
-                                            {flow_t::WHILE_LOOP, FlowControl()},
-                                            {flow_t::ROUTINE, FlowControl()},
-                                            {flow_t::CALL, FlowControl()} };
+    //   /// Mapping from flow type to flow control structure.
+    //   // todo - could change this to an array because we know things at compile time
+    //   std::map<flow_t, FlowControl> lib = { {flow_t::BASIC, FlowControl()},
+    //                                         {flow_t::WHILE_LOOP, FlowControl()},
+    //                                         {flow_t::ROUTINE, FlowControl()},
+    //                                         {flow_t::CALL, FlowControl()} };
 
-      FlowControl & operator[](flow_t type) {
-        emp_assert(Has(lib, type), "FlowType not recognized!");
-        return lib[type];
-      }
+    //   FlowControl & operator[](flow_t type) {
+    //     emp_assert(Has(lib, type), "FlowType not recognized!");
+    //     return lib[type];
+    //   }
 
-      const FlowControl & operator[](flow_t type) const {
-        emp_assert(Has(lib, type), "FlowType not recognized!");
-        return lib[type];
-      }
+    //   const FlowControl & operator[](flow_t type) const {
+    //     emp_assert(Has(lib, type), "FlowType not recognized!");
+    //     return lib[type];
+    //   }
 
-      std::string FlowTypeToString(flow_t type) const {
-        switch (type) {
-          case flow_t::BASIC: return "BASIC";
-          case flow_t::WHILE_LOOP: return "WHILE_LOOP";
-          case flow_t::ROUTINE: return "ROUTINE";
-          case flow_t::CALL: return "CALL";
-          default: return "UNKNOWN";
-        }
-      }
+    //   std::string FlowTypeToString(flow_t type) const {
+    //     switch (type) {
+    //       case flow_t::BASIC: return "BASIC";
+    //       case flow_t::WHILE_LOOP: return "WHILE_LOOP";
+    //       case flow_t::ROUTINE: return "ROUTINE";
+    //       case flow_t::CALL: return "CALL";
+    //       default: return "UNKNOWN";
+    //     }
+    //   }
 
-      void OpenFlow(this_t & hw, const flow_info_t & new_flow, exec_state_t & state) {
-        flow_t type = new_flow.type;
-        emp_assert(Has(lib, type), "FlowType not recognized!");
-        lib[type].open_flow_fun(hw, state, new_flow);
-      }
+    //   void OpenFlow(this_t & hw, const flow_info_t & new_flow, exec_state_t & state) {
+    //     flow_t type = new_flow.type;
+    //     emp_assert(Has(lib, type), "FlowType not recognized!");
+    //     lib[type].open_flow_fun(hw, state, new_flow);
+    //   }
 
-      void CloseFlow(this_t & hw, flow_t type, exec_state_t & state) {
-        emp_assert(Has(lib, type), "FlowType not recognized!");
-        lib[type].close_flow_fun(hw, state);
-      }
+    //   void CloseFlow(this_t & hw, flow_t type, exec_state_t & state) {
+    //     emp_assert(Has(lib, type), "FlowType not recognized!");
+    //     lib[type].close_flow_fun(hw, state);
+    //   }
 
-      void BreakFlow(this_t & hw, flow_t type, exec_state_t & state) {
-        emp_assert(Has(lib, type), "FlowType not recognized!");
-        lib[type].break_flow_fun(hw, state);
-      }
-    };
+    //   void BreakFlow(this_t & hw, flow_t type, exec_state_t & state) {
+    //     emp_assert(Has(lib, type), "FlowType not recognized!");
+    //     lib[type].break_flow_fun(hw, state);
+    //   }
+    // };
 
     /// Module definition.
     struct Module {
@@ -257,7 +164,7 @@ namespace emp { namespace signalgp {
 
   protected:
     emp::Ptr<inst_lib_t> inst_lib;  ///< Library of program instructions.
-    FlowHandler flow_handler;       ///< The flow handler manages the behavior of different types of execution flow.
+    flow_handler_t flow_handler;       ///< The flow handler manages the behavior of different types of execution flow.
     memory_model_t memory_model;    ///< The memory model manages any global memory state and specifies call state memory.
     program_t program;              ///< Program loaded on this execution stepper.
     emp::vector<module_t> modules;  ///< List of modules in program.
@@ -530,7 +437,7 @@ namespace emp { namespace signalgp {
     /// Get reference to random number generator used by this hardware.
     Random & GetRandom() { return random; }
 
-    FlowHandler & GetFlowHandler() { return flow_handler; }
+    flow_handler_t & GetFlowHandler() { return flow_handler; }
 
     /// Set open flow handler for given flow type.
     void SetOpenFlowFun(flow_t type, const fun_open_flow_t & fun) {
--- source/linear_signalgp_utils.h
+++ source/linear_signalgp_utils.h
@@ -43,6 +43,64 @@ namespace emp { namespace signalgp { namespace lsgp_utils {
     FlowType GetType() const { return type; }
   };
 
+  // NEED: hardware_t, flow_t, exec_state_t,
+  template<typename HARDWARE_T, typename EXEC_STATE_T>
+  struct FlowHandler {
+    using exec_state_t = EXEC_STATE_T;
+    using hardware_t = HARDWARE_T;
+    using fun_end_flow_t = std::function<void(hardware_t&, exec_state_t &)>;
+    using fun_open_flow_t  = std::function<void(hardware_t&, exec_state_t &, const FlowInfo &)>;
+
+    struct FlowControl {
+      fun_open_flow_t open_flow_fun;
+      fun_end_flow_t close_flow_fun;
+      fun_end_flow_t break_flow_fun;
+    };
+
+    /// Mapping from flow type to flow control structure.
+    // todo - could change this to an array because we know things at compile time
+    std::map<FlowType, FlowControl> lib = {{FlowType::BASIC, FlowControl()},
+                                           {FlowType::WHILE_LOOP, FlowControl()},
+                                           {FlowType::ROUTINE, FlowControl()},
+                                           {FlowType::CALL, FlowControl()} };
+
+    FlowControl & operator[](FlowType type) {
+      emp_assert(Has(lib, type), "FlowType not recognized!");
+      return lib[type];
+    }
+
+    const FlowControl & operator[](FlowType type) const {
+      emp_assert(Has(lib, type), "FlowType not recognized!");
+      return lib[type];
+    }
+
+    std::string FlowTypeToString(FlowType type) const {
+      switch (type) {
+        case FlowType::BASIC: return "BASIC";
+        case FlowType::WHILE_LOOP: return "WHILE_LOOP";
+        case FlowType::ROUTINE: return "ROUTINE";
+        case FlowType::CALL: return "CALL";
+        default: return "UNKNOWN";
+      }
+    }
+
+    void OpenFlow(hardware_t & hw, const FlowInfo & new_flow, exec_state_t & state) {
+      FlowType type = new_flow.type;
+      emp_assert(Has(lib, type), "FlowType not recognized!");
+      lib[type].open_flow_fun(hw, state, new_flow);
+    }
+
+    void CloseFlow(hardware_t & hw, FlowType type, exec_state_t & state) {
+      emp_assert(Has(lib, type), "FlowType not recognized!");
+      lib[type].close_flow_fun(hw, state);
+    }
+
+    void BreakFlow(hardware_t & hw, FlowType type, exec_state_t & state) {
+      emp_assert(Has(lib, type), "FlowType not recognized!");
+      lib[type].break_flow_fun(hw, state);
+    }
+  };
+
   /// State information for a function call.
   template<typename MEMORY_STATE_T>
   struct CallState {
