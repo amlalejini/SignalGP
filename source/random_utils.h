1bbc27db329f142f69ac7d27c48fc81678c66be8
Alex Lalejini <amlalejini@gmail.com>
2019-11-13 18:23:12 -0500

    add random utilities for generating bitsets + tests

---
+++ source/random_utils.h
@@ -0,0 +1,84 @@
+/**
+ *  @note This file is part of Empirical, https://github.com/devosoft/Empirical
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2018
+ *
+ *  @file  signalgp_utils.h
+ *  @brief Helper functions for working with SignalGP virtual hardware/programs.
+ *  @todo Mutator class
+ *  @todo tests
+ */
+
+#ifndef SGP_BITSET_UTILS_H
+#define SGP_BITSET_UTILS_H
+
+#include <unordered_set>
+#include <string>
+#include <functional>
+#include <algorithm>
+
+#include "base/errors.h"
+#include "hardware/EventDrivenGP.h"
+#include "tools/BitSet.h"
+#include "tools/math.h"
+#include "tools/Random.h"
+#include "tools/random_utils.h"
+#include "tools/map_utils.h"
+
+namespace emp {
+
+  /// Generate one random BitSet<W>.
+  /// Given a vector of other BitSets (unique_from), this function will guarantee
+  /// the generated BitSet is unique with respect to those BitSets.
+  /// @param rnd - Random number generator to use.
+  /// @param unique_from - Other BitSets that the generated BitSet should be unique from.
+  template<size_t W>
+  BitSet<W> RandomBitSet(emp::Random & rnd, const emp::vector<BitSet<W>> & unique_from=emp::vector<BitSet<W>>()) {
+    std::unordered_set<BitSet<W>> unique_from_set(unique_from.begin(), unique_from.end());
+    emp_assert(unique_from_set.size() < emp::Pow2(W), "BitSet<W> is not large enough to be able to guarantee requested number of unique tags");
+    BitSet<W> new_bitset(rnd, 0.5);
+    while (unique_from_set.size()) {
+      if (emp::Has(unique_from_set, new_bitset)) {
+        new_bitset.Randomize(rnd);
+        continue;
+      }
+      break;
+    }
+    return new_bitset;
+  }
+
+  /// Generate 'count' number of random SignalGP tags (BitSet<TAG_WIDTH>).
+  /// Given a vector of other tags (unique_from), this function will guarantee the tags generated are unique with respect to those tags.
+  /// @param rnd - Random number generator to use when generating a random tag.
+  /// @param count - How many tags should be generated?
+  /// @param guarantee_unique - Should generated tags be guaranteed to be unique from each other?
+  /// @param unique_from - Other tags that the tag being generated should be unique with respect to. Only used if 'guarantee_unique' is true.
+  template<size_t W>
+  emp::vector<BitSet<W>> RandomBitSets(emp::Random & rnd, size_t count, bool guarantee_unique=false,
+                                       const emp::vector<BitSet<W>> & unique_from=emp::vector<BitSet<W>>())
+  {
+    // If we don't have to make any promises, run quickly!
+    emp::vector<BitSet<W>> new_bitsets(count);
+    if (!guarantee_unique && unique_from.size() == 0) {
+      for (auto & bs : new_bitsets) { bs.Randomize(rnd); }
+    } else {
+      std::unordered_set<BitSet<W>> unique_from_set(unique_from.begin(), unique_from.end());
+      emp_assert(unique_from_set.size()+count <= emp::Pow2(W), "Not possible to generate requested number of BitSets");
+      for (auto & bs : new_bitsets) {
+        bs.Randomize(rnd);
+        while (unique_from_set.size()) {
+          if (emp::Has(unique_from_set, bs)) {
+            bs.Randomize(rnd);
+            continue;
+          }
+          break;
+        }
+        unique_from_set.emplace(bs);
+      }
+    }
+    return new_bitsets;
+  }
+
+}
+
+#endif
--- tests/unit_tests.cc
+++ tests/unit_tests.cc
@@ -17,9 +17,113 @@
 #include "utils/linear_functions_program_instructions_impls.h"
 #include "utils/MemoryModel.h"
 #include "utils/LinearFunctionsProgram.h"
+#include "../source/random_utils.h"
 
 TEST_CASE( "Hello World", "[general]" ) {
   std::cout << "Hello tests!" << std::endl;
+}
+
+TEST_CASE("RandomBitSet<W>") {
+  constexpr size_t RANDOM_SEED = 1;
+  emp::Random random(RANDOM_SEED);
+  std::unordered_set<uint32_t> uset; // Will be used to double-check uniqueness.
+
+  // Generate a bunch of big random tags. No uniqueness guarantees.
+  for (size_t i = 0; i < 100; ++i) auto tag = emp::RandomBitSet<1024>(random);
+
+  // Enumerate all 2-bit tags
+  emp::vector<emp::BitSet<2>> tags2;
+  uset.clear();
+  // std::cout << "All two-bit tags: " << std::endl;
+  for (size_t i = 0; i < emp::Pow2(2); ++i) {
+    tags2.emplace_back(emp::RandomBitSet<2>(random, tags2));
+    uset.emplace(tags2.back().GetUInt(0));
+    // std::cout << "  ";
+    // tags2.back().Print();
+    // std::cout << " : " << tags2.back().GetUInt(0) << std::endl;
+  }
+  REQUIRE(tags2.size() == emp::Pow2(2));
+  REQUIRE(uset.size() == emp::Pow2(2));
+  for (size_t i = 0; i < emp::Pow2(2); ++i) REQUIRE(emp::Has(uset, i));
+
+  // Enumerate all 4-bit tags
+  emp::vector<emp::BitSet<4>> tags4;
+  uset.clear();
+  // std::cout << "All four-bit tags: " << std::endl;
+  for (size_t i = 0; i < emp::Pow2(4); ++i) {
+    tags4.emplace_back(emp::RandomBitSet<4>(random, tags4));
+    uset.emplace(tags4.back().GetUInt(0));
+    // std::cout << "  ";
+    // tags4.back().Print();
+    // std::cout << " : " << tags4.back().GetUInt(0) << std::endl;
+  }
+  REQUIRE(tags4.size() == emp::Pow2(4));
+  REQUIRE(uset.size() == emp::Pow2(4));
+  for (size_t i = 0; i < emp::Pow2(4); ++i) REQUIRE(emp::Has(uset, i));
+
+  // Generate a bunch of 16-bit tags.
+  emp::vector<emp::BitSet<16>> tags16;
+  for (size_t k = 0; k < 100; ++k) {
+    uset.clear();
+    tags16.clear();
+    for (size_t i = 0; i < 1000; ++i) {
+      tags16.emplace_back(emp::RandomBitSet<16>(random, tags16));
+      uset.emplace(tags16.back().GetUInt(0));
+    }
+    REQUIRE(uset.size() == 1000);
+  }
+}
+
+TEST_CASE("RandomBitSets<W>") {
+  constexpr size_t RANDOM_SEED = 1;
+  emp::Random random(RANDOM_SEED);
+
+  std::unordered_set<uint32_t> uset; // Will be used to double-check uniqueness.
+
+  // Generate lots of small tags with no guarantees on uniqueness.
+  auto small_tags = emp::RandomBitSets<2>(random, 1000);
+  REQUIRE(small_tags.size() == 1000);
+  // Generate lots of large tags with no guarantees on uniqueness.
+  auto big_tags = emp::RandomBitSets<1024>(random, 1000);
+  REQUIRE(big_tags.size() == 1000);
+
+  // Use generator to enumerate all 2-bit tags.
+  auto tags2 = emp::RandomBitSets<2>(random, emp::Pow2(2), true);
+  uset.clear();
+  for (size_t i = 0; i < tags2.size(); ++i) {
+    uset.emplace(tags2[i].GetUInt(0));
+  }
+  REQUIRE(tags2.size() == emp::Pow2(2));
+  REQUIRE(uset.size() == emp::Pow2(2));
+  for (size_t i = 0; i < emp::Pow2(2); ++i) REQUIRE(emp::Has(uset, i));
+
+  // Use generator to enumerate all 4-bit tags.
+  auto tags4 = emp::RandomBitSets<4>(random, emp::Pow2(4), true);
+  uset.clear();
+  for (size_t i = 0; i < tags4.size(); ++i) {
+    uset.emplace(tags4[i].GetUInt(0));
+  }
+  REQUIRE(tags4.size() == emp::Pow2(4));
+  REQUIRE(uset.size() == emp::Pow2(4));
+  for (size_t i = 0; i < emp::Pow2(4); ++i) REQUIRE(emp::Has(uset, i));
+
+  // Generate a bunch of 8-bit tags (50% of the tag-space).
+  // Check for uniqueness.
+  auto tags8 = emp::RandomBitSets<8>(random, 128, true);
+  uset.clear();
+  for (size_t i = 0; i < tags8.size(); ++i) uset.emplace(tags8[i].GetUInt(0));
+  REQUIRE(tags8.size() == 128);
+  REQUIRE(uset.size() == 128);
+
+  // Generate a bunch of 8-bit tags using previously generated tag as
+  // reserved tag-space. Check for proper uniqueness.
+  for (size_t i = 0; i < 100; ++i) {
+    std::unordered_set<uint32_t> temp_set(uset);
+    auto tags = emp::RandomBitSets<8>(random, 64, true, tags8);
+    REQUIRE(tags.size() == 64);
+    for (size_t t = 0; t < tags.size(); ++t) temp_set.emplace(tags[t].GetUInt(0));
+    REQUIRE(temp_set.size() == 128+64);
+  }
 }
 
 TEST_CASE("Toy SignalGP", "[general]") {
@@ -273,7 +377,6 @@ TEST_CASE("Linear Functions Program") {
   using tag_t = emp::BitSet<8>;
   using arg_t = int;
   using program_t = emp::signalgp::LinearFunctionsProgram<tag_t, arg_t>;
-
   program_t program1;
 }
 
